ad2d2640f3407249dd8cdcadd604843e2685f611
BishalHD <112864723+BishalHD@users.noreply.github.com>
2022-12-06 10:46:05 +0000

    Update fibonacci sequence.py

--- Brampton Manor Academy/Fibonacci/fibonacci sequence.py
+++ Brampton Manor Academy/Fibonacci/fibonacci sequence.py
@@ -1,43 +1,51 @@
 import json
 
 def fibonacci(array):
-    for i in range(0,30):
+    for i in range(0,33):
         if i > 1:
             array.append(array[i-2]+array[i-1])
         else:
             array.append(i)
     return array
 
-def check(array, x, calculation,):
+def check(array, user_input, num, calculation):
     for item in array:
-        if item<= x:
+        if item<= user_input:
             sublist = []
             sublist.append(item)
             num = sublist[0]
     calculation = 100 - int(num)
     return num, calculation
 
+def loop(array, calculation, num):
+    final_list = []
+    for each in array:
+        if calculation == each:
+            final_list.append(num)
+        else:
+            user_input = calculation
+            num, calculation = check(array, user_input, num, calculation)
+            final_list.append(num)
+    return final_list
+
+
+
 if __name__ == "__main__":
     array = []
+    num = 0
     calculation = 0
-    x = int(input("..."))
-    sequence = fibonacci(array)
+    user_input = int(input("..."))
+    sequence =fibonacci(array)
 
-    with open('Fibonacci_sequence', 'w') as f:
-        f.write(json.dumps(sequence))
+    #with open('Fibonacci_sequence', 'w') as f:
+        #f.write(json.dumps(sequence))
 
-    with open('Fibonacci_sequence', 'r') as f:
-        sequence = json.loads(f.read())
+    #with open('Fibonacci_sequence', 'r') as f:
+        #sequence = json.loads(f.read())
 
-    z = check(array, x, calculation = None)
-    z = str(z)
-    z = z[1]
-    print(z)
-    for j in range (len(array)):
-        while z[1]!= array[j]:
-            z = check(array, x, calculation = None)
-            z = str(z)
-            z = z[1]
-            print(z)
-            
-    
+    num, calculation = check(array, user_input, num, calculation)
+    print(num)
+    print(calculation)
+    final_list = loop(array, calculation, num)
+    print (final_list)
+
